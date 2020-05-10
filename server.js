b98217fc465f941fc58de07c8d2ca04f8704030a
saphirah.pociluyko <sjpcp5@gmail.com>
2020-05-10 04:56:31 -0500

    app loads and db is seeded

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const logger = require('morgan')
 const mongoose = require('mongoose')
-const routes = require('./routes')
 const app = express()
 const PORT = process.env.PORT || 3001
 
@@ -11,17 +10,14 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 // Serve up static assets (usually on heroku)
 app.use(express.static('public'))
-
+const db = require('./models')
 // Add routes, both API and view
-app.use(routes)
-
+require('./routes/api-routes')(app)
+require('./routes/html-routes')(app)
 // Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/workout',
-  {
-    useNewUrlParser: true
-  }
-)
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+  useNewUrlParser: true
+})
 
 // Start the API server
 app.listen(PORT, function () {
